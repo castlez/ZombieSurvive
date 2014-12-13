76fbf0d3c5ed75cf4c7e6de459f2ffc3bce75899
castlez <castlez93@gmail.com>
2014-12-13 00:29:51 -0800

    added player and room classes

---
+++ player.h
@@ -0,0 +1,17 @@
+//ZOMBIE SURVIVE
+//Jonny Castle
+//
+//Interface for a character
+//holds all information about
+//the players as well as actions
+//available
+//
+
+
+
+class player{
+  public:
+    player();
+    ~player();
+
+}
